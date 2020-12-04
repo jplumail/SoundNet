a9c00786c505f6779b0727d89139744cb317264d
jplumail <jeanplumail76@gmail.com>
2020-12-04 10:18:53 +0100

    Added plotting in gridsearch training

--- svm.py
+++ svm.py
@@ -2,6 +2,7 @@ import pickle
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from time import time
 from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import make_pipeline
@@ -23,18 +24,18 @@ def save_model(clf, layer):
     return model_path
 
 def training(db, layer):
-    from time import time
     t1 = time()
     print("Loading training data...")
-    X, y = get_training_data(db, layer, n_jobs=2)
+    X, y, _ = get_training_data(db, layer, n_jobs=4)
     print(time()-t1, " seconds")
 
     cv = get_k_fold(db)
     pipeline = make_pipeline(StandardScaler(), SVC())
+    C_range = np.logspace(-2, 7, num=5)
+    gamma_range = np.logspace(-7, -2, num=5)
     parameters ={
-        "svc__C": np.logspace(1.5, 2.5, num=3),
-        "svc__gamma": np.logspace(-3.5, -2.5, num=3),
-        "svc__kernel": ["rbf"]
+        "svc__C": C_range,
+        "svc__gamma": gamma_range,
     }
     clf = GridSearchCV(pipeline, parameters, cv=cv, n_jobs=4, refit=True, verbose=0)
     
@@ -46,21 +47,26 @@ def training(db, layer):
     print("Best parameters found : ")
     print(clf.best_params_)
 
-    means = clf.cv_results_['mean_test_score']
-    stds = clf.cv_results_['std_test_score']
-    for mean, std, params in zip(means, stds, clf.cv_results_['params']):
-        print("%0.3f (+/-%0.03f) for %r"
-              % (mean, std * 2, params))
-    
-    save_model(clf, layer)
-    
+    scores = clf.cv_results_['mean_test_score'].reshape(len(C_range),
+                                                     len(gamma_range))
+    plt.figure(figsize=(8, 6))
+    plt.subplots_adjust(left=.2, right=0.95, bottom=0.15, top=0.95)
+    plt.imshow(scores, interpolation='nearest', cmap=plt.cm.hot)
+    plt.xlabel('gamma')
+    plt.ylabel('C')
+    plt.colorbar()
+    plt.xticks(np.arange(len(gamma_range)), ["%.2e"%f for f in gamma_range], rotation=45)
+    plt.yticks(np.arange(len(C_range)), ["%.2e"%f for f in C_range])
+    plt.title('Validation accuracy')
+    plt.savefig("training-layer={}.png".format(layer), format="png")
+    save_model(clf,layer)
+
     return clf
 
 def evaluating(db, clf, layer):
-    from time import time
     print("Loading test data...")
     t0 = time()
-    X_test, y_test = get_test_data(db, layer, n_jobs=2)
+    X_test, y_test, _ = get_test_data(db, layer, n_jobs=4)
     print(time()-t0)
     y_pred = clf.predict(X_test)
     print(classification_report(y_test, y_pred))
@@ -72,7 +78,7 @@ def evaluating(db, clf, layer):
 if __name__ == "__main__":
     db = load_DCASE_development()
     features_extraction_DCASE(db)
-    for layer in range(3,9):
+    for layer in range(3, 9):
         print("#############")
         print("LAYER : ", layer)
         clf = training(db, layer)
